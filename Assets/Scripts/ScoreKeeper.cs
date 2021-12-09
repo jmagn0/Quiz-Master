55495b0aa2ae1e25ff3ad35fabc35891b33b452c
Magno Dutra <jmagno.dutra@hotmail.com>
2021-12-08 22:54:00 -0300

    Init commit

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    int correctAnswer = 0;
+    int questionsSeen = 0;
+
+    public int GetCorrectAnswers()
+    {
+        return correctAnswer;
+    }
+
+    public void IncrementCorrectAnswers()
+    {
+        correctAnswer++;
+    }
+
+    public int QuestionSeen()
+    {
+        return questionsSeen;
+    }
+
+    public void IncrementQuestionSeen()
+    {
+        questionsSeen++;
+    }
+
+    public int CalculateScore()
+    {
+        return Mathf.RoundToInt(correctAnswer / (float)questionsSeen * 100);
+    }
+}
